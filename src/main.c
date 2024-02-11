6f33915e27ba27ba4d4fbd19b123f4b0600864fb
Jake <jacobbutfiloski@gmail.com>
2024-02-11 02:49:13 -0500

    Added MacOS compatability

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 #include "Engine/model.c"
 #include "Engine/input.c"
 
+
 #include "world.c"
 
 #include <cglm.h>
@@ -30,6 +31,12 @@ int main()
         printf("Failed to init GLFW");
         return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on Mac
+
 
     //Create Main OpenGL GLFW Window
     InitWindow();
@@ -61,17 +68,24 @@ int main()
     //Bind buffer callback so viewport resizes with window
     glfwSetFramebufferSizeCallback(w->skeleton->window, framebuffer_size_callback);
     glm_perspective(glm_rad(45.0f), WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f, globalProjectionMatrix);
-
     //Shaders
-    GLuint vertex_shader = compile_vertex_shader("shaders/vertex_shader.glsl");
-    GLuint fragment_shader = compile_fragment_shader("shaders/fragment_shader.glsl");
+    #ifdef _WIN32
+    GLuint vertex_shader = compile_vertex_shader("shaders/vertex_shader_win64.glsl");
+    GLuint fragment_shader = compile_fragment_shader("shaders/fragment_shader_win64.glsl");
+    #endif
 
+    #if defined(__APPLE__) && defined(__MACH__)
+    GLuint vertex_shader = compile_vertex_shader("shaders/vertex_shader_osx.glsl");
+    GLuint fragment_shader = compile_fragment_shader("shaders/fragment_shader_osx.glsl");
+    #endif
     GLuint shader_program = create_shader_program(vertex_shader, fragment_shader);
+    
     glUseProgram(shader_program);
     
     //Delete shaders now that they've been linked to the program
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
+    
 
     InitCamera(&camera, (vec3){2.0f, CHUNK_HEIGHT, 10.0f});
     UpdateCameraVectors(&camera);
@@ -87,10 +101,11 @@ int main()
     //This has to be here bc idk, it was in window create in gfx.c before but it just wasnt working lol
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
-
+    
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glfwWindowHint(GLFW_DEPTH_BITS, 24);
+    
     //Buffer Loop
     float lastFrame = 0.0f;
     while(!glfwWindowShouldClose(w->skeleton->window))
@@ -117,7 +132,6 @@ int main()
         float green = sin(deltaTime) / 2.0f + 0.5f;
         int vertexColorLocation = glGetUniformLocation(shader_program, "vertexColor");
         glUniform4f(vertexColorLocation, 0.0f, green, 0.0f, 1.0f);
-
         //Draw Code Here
         DrawWorld(world, shader_program, textureAtlas);
         //
@@ -134,7 +148,7 @@ int main()
 
 void CalculateFrameRate()
 {
-    static float framesPerSecond = 0.0f;
+    /*static float framesPerSecond = 0.0f;
     static int fps;
     static float lastTime = 0.0f;
     float currentTime = GetTickCount() * 0.001f;
@@ -145,6 +159,6 @@ void CalculateFrameRate()
         lastTime = currentTime;
         fps = (int)framesPerSecond;
         framesPerSecond = 0;
-    }
+    }*/
 }
 
